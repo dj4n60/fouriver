e7bb678611231abf919d183f8225dc4e672f2849
Elias999 <p17051@students.cs.unipi.gr>
2020-05-07 11:34:29 +0300

    Still first commit

--- fouriver/urls.py
+++ fouriver/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('index/', include('site_app.urls')),
+    path('', include('site_app.urls')),
     path('admin/', admin.site.urls),
 ]
 
