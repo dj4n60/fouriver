af99b69d001bba844ba76f7f352433599a4cb459
chimichangas99 <sperako948@gmail.com>
2020-06-19 15:52:56 +0300

    In progress..
    change templates to EditCustomerInfo.html and EditDeveloperInfo.html
    create forms.py and added django forms to handle the edit process
    changed a variety of things on models,urls,views for everything to work
    fixed data saved after edit (not profile pic though)
    fixed edited info showing as <queryset[]>
    
    need to do: fix appearance on edit profile page, make the location shown on profile page be the correct one if you have edited it ,fix the file upload

--- projects/Calls.py
+++ projects/Calls.py
@@ -13,7 +13,7 @@ class Calls():
         user1 = appusers()
         result = appusers.objects.filter(username=request.session.get("username"))
 
-        if request.session.get('idiotita') == 'developer' :
+        if request.session.get('idiotita') == 'developer':
             result1 = developerinfo.objects.filter(username=request.session.get("username"))
 
             user1.info1 = result1.values_list('github',flat=True)[0]
@@ -25,19 +25,19 @@ class Calls():
         else:
             result2 = customerinfo.objects.filter(username=request.session.get("username"))
 
-            user1.info1 = result2.get('linkedin')
-            user1.info2 = result2.get('disc')
+            user1.info1 = result2.values_list('linkedin', flat=True)[0]
+            user1.info2 = result2.get('disc', flat=True)[0]
             user1.info3 = " "
-            user1.profile_pic = result2.get('profile_pic')
-            user1.location2 = result2.get('location')
+            user1.profile_pic = result2.get('profile_pic', flat=True)[0]
+            user1.location2 = result2.get('location', flat=True)[0]
 
         user1.username = result.values_list('fullname', flat=True)[0]
-        user1.location = result.values_list('location', flat=True)[0]  #result.location
-        user1.birthday = result.values_list('birthday', flat=True)[0] #result.birthday
-        user1.gmail = result.values_list('email', flat=True)[0] #result.birthday
-        user1.twlink = 'test' #result.twlink
-        user1.fblink = 'test' #result.fblink
-        user1.gitlink = 'test' #result.gitlink
+        user1.location = result.values_list('location', flat=True)[0]
+        user1.birthday = result.values_list('birthday', flat=True)[0]
+        user1.gmail = result.values_list('email', flat=True)[0]
+        user1.twlink = 'test'
+        user1.fblink = 'test'
+        user1.gitlink = 'test'
 
         if user1.location2 in globals():
             user1.location = user1.location2
